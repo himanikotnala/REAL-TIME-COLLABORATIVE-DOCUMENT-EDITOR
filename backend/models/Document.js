cb6f1bbed9fd99d013e115019752b355999f1912
himanikotnala <himanikotnala8126@gmail.com>
2025-07-04 16:33:23 +0530

    Update Document.js

--- backend/models/Document.js
+++ backend/models/Document.js
@@ -1,1 +1,11 @@
+/ backend/models/Document.js
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  _id: String,
+  data: Object,
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
+
 
